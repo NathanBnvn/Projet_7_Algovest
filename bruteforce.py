722544adf9f4a55d466cbfac376924db66e0c3c7
Nathan Bienvenu <45998296+NathanBnvn@users.noreply.github.com>
2021-08-17 15:02:42 +0200

    Correction de l'algo de force brute
    Changement du calcul du bénéfice.
    Optimisation de l'affichage des actions du portefeuille calculé

--- bruteforce.py
+++ bruteforce.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 
 import csv
+import itertools as it
 
 
 MAX_COST = 500
+y = []
 
 def read_dataset():
 	raw_interests = []
@@ -28,24 +30,29 @@ def read_dataset():
 
 def get_benefits(ACTIONS, COST, INTEREST_RATE):
 	estimations = {'actions': [], 'cost': [], 'benefit': []}
+	#create_csv_file()
 
 	for n in range(0, len(COST)):
 		cost = COST[n]
 		action = ACTIONS[n]
 		interest = INTEREST_RATE[n]
+		benefit = cost * interest / 100
+		actions = []
 
 		while cost < MAX_COST:
 			for i in range(0, len(COST)):
 				if not COST[n] == COST[i]:
 					cost = cost + COST[i]
-					action_set = action + ' à ' + ACTIONS[i]
-					interest = interest + INTEREST_RATE[i]
-					benefit = cost * interest / 100
+					benefit = COST[i] * INTEREST_RATE[i] / 100 + benefit
+					actions.append(action + ': ' + ACTIONS[i])
+
 					if cost < MAX_COST:
-						estimations['actions'].append(action_set) 
-						estimations['cost'].append(cost) 
+						estimations['actions'].append(actions)
+						estimations['cost'].append(cost)
 						estimations['benefit'].append(benefit)
+
 	get_max_value(estimations)
+
 
 
 def get_max_value(estimations):
@@ -53,8 +60,15 @@ def get_max_value(estimations):
 	max_value_index = estimations['benefit'].index(max_value)
 	actions = estimations['actions'][max_value_index]
 	costs = estimations['cost'][max_value_index]
-	print(actions + ',  coût:' + str(costs) + ',  bénéfice:' + str(max_value))
+	print('actions:' + str(actions) + ',  coût:' + str(costs) + ',  bénéfice:' + str(max_value))
 
+def create_csv_file():
+	with open('estimates.csv', 'w', encoding='utf-8') as estimation_file:
+		estimation_file.write("ACTIONS, COÛT\n")
+
+def save_estimates(estimation):
+	with open('estimates.csv', 'a', encoding='utf-8') as estimation_file:
+		estimation_file.write(str(estimation['actions']) + str(estimation['cost']) + '\n')
 
 def main():
 	read_dataset()

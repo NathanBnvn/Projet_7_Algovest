7d6e5cddd18fc9cad81762581ff6bfa7cd83bf47
Nathan Bienvenu <45998296+NathanBnvn@users.noreply.github.com>
2021-09-22 12:17:23 +0200

    MAJ
    tentative d’intégration des float

--- optimized.py
+++ optimized.py
@@ -10,7 +10,9 @@ def read_dataset():
     ACTIONS = []
     estimations = {'actions': [], 'cost': [], 'benefit': []}
 
-    with open('dataset.csv', 'r', encoding='utf-8') as csv_dataset:
+    select_dataset = input('Enter the dataset file name : ')
+
+    with open(select_dataset + '.csv', 'r', encoding='utf-8') as csv_dataset:
         csv_reader = csv.reader(csv_dataset)
         for row in csv_reader:
             ACTIONS.append(row[0])
@@ -21,23 +23,26 @@ def read_dataset():
         raw_costs.pop(0)
         raw_interests.pop(0)
 
-        COST = [int(cost) for cost in raw_costs]
-        INTEREST_RATE = [int(interest) for interest in raw_interests]
+        COST = [int(float(cost)) for cost in raw_costs]
+
+        INTEREST_RATE = [int(float(interest)) for interest in raw_interests]
         BENEFIT = []
 
-        for i in range(20):
+        for i in range(len(COST)):
             benefit = COST[i] * INTEREST_RATE[i] / 100
             BENEFIT.append(benefit)
 
         BENEFIT_NUMBER = len(BENEFIT)
+
         for x in range(BENEFIT_NUMBER):
             estimations['actions'].append(ACTIONS[x])
             estimations['cost'].append(COST[x])
             estimations['benefit'].append(BENEFIT[x])
 
-        print(knapSack(MAX_CAPACITY, estimations, BENEFIT_NUMBER))
+        
+        print(stockPortfolio(MAX_CAPACITY, estimations, BENEFIT_NUMBER))
 
-def knapSack(capacity, estimations, o):
+def stockPortfolio(capacity, estimations, benefit_number):
 
     matrix = [[0 for x in range(capacity + 1)] for x in range(len(estimations['actions']) + 1)]
 
@@ -49,16 +54,15 @@ def knapSack(capacity, estimations, o):
                 matrix[i][w] = matrix[i-1][w]
 
     selected_elements = []
-    o = len(estimations['actions'])
+    benefit_number = len(estimations['actions'])
 
-    while o > 0:
-        e = estimations['actions'][o-1]
-        print(e)
-        if matrix[o][w] == matrix[o-1][w-estimations['cost'][o-1]] + estimations['benefit'][o-1]:
+    while benefit_number > 0:
+        e = estimations['actions'][benefit_number-1]
+        if matrix[benefit_number][w] == matrix[benefit_number-1][w-estimations['cost'][benefit_number-1]] + estimations['benefit'][benefit_number-1]:
             selected_elements.append(e)
-            w -= estimations['cost'][o-1]
+            w -= estimations['cost'][benefit_number-1]
 
-        o -= 1
+        benefit_number -= 1
 
     return matrix[-1][-1], selected_elements
 
